3d2c41b4e7946f0584f1a3303c9b0e1dea8a01d5
LemonTreeLT <108186846+LemonTreeLT@users.noreply.github.com>
2023-09-09 21:36:29 +0800

    SomeTools

--- src/client/java/net/lemontree/SomeToolsModClient.java
+++ src/client/java/net/lemontree/SomeToolsModClient.java
@@ -3,8 +3,9 @@ package net.lemontree;
 import net.fabricmc.api.ClientModInitializer;
 
 public class SomeToolsModClient implements ClientModInitializer {
+
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+
 	}
 }
