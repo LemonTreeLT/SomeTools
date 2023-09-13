a059e03aca8cf97d1617beb5aff361619f69fbe7
LemonTreeLT <108186846+LemonTreeLT@users.noreply.github.com>
2023-09-14 00:43:52 +0800

    cls command complete!

---
+++ src/main/java/net/lemontree/Commands.java
@@ -0,0 +1,21 @@
+package net.lemontree;
+
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.lemontree.model.ClearDrops;
+
+import static com.mojang.brigadier.arguments.IntegerArgumentType.getInteger;
+import static com.mojang.brigadier.arguments.IntegerArgumentType.integer;
+import static net.minecraft.server.command.CommandManager.argument;
+import static net.minecraft.server.command.CommandManager.literal;
+
+public class Commands {
+    public static void regClsCommand() {
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) ->
+                dispatcher.register(literal("cls")
+                        .requires(source -> source.hasPermissionLevel(1))
+                        .then(argument("time", integer())
+                                .executes(ctx -> ClearDrops.ClsDrops(getInteger(ctx, "time"), ctx.getSource())))
+                        .executes(source -> ClearDrops.ClsDrops(-1, source.getSource()))
+                ));
+    }
+}
